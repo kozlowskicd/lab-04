ea6e379346d9cd9d8415f86a2a35c5077b7bb0a2
Chris Kozlowski <kozlowskicd@gmail.com>
2018-10-15 18:42:55 -0700

    initial commit

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 const fs = require('fs');
 
+
 /**
  * Bitmap -- receives a file name, used in the transformer to note the new buffer
  * @param filePath
@@ -9,15 +10,19 @@ const fs = require('fs');
  */
 function Bitmap(filePath) {
   this.file = filePath;
+  let buffer = Buffer.from(filePath);
 }
 
 /**
- * Parser -- accepts a buffer and will parse through it, according to the specification, creating object properties for each segment of the file
+ * Parser -- accepts a buffer and will parse through it, according to the specification, creating object properties for each segment of the fileb
  * @param buffer
  */
 Bitmap.prototype.parse = function(buffer) {
+  
   this.type = buffer.toString('utf-8', 0, 2);
-  //... and so on
+  this.width = buffer.readInt16LE(18);
+  this.height = buffer.readInt16LE(22);
+  this.numberOfColors = buffer.readInt16LE(46);
 };
 
 /**
@@ -28,6 +33,7 @@ Bitmap.prototype.transform = function(operation) {
   // This is really assumptive and unsafe
   transforms[operation](this);
   this.newFile = this.file.replace(/\.bmp/, `.${operation}.bmp`);
+  console.log(this.newFile);
 };
 
 /**
@@ -36,16 +42,18 @@ Bitmap.prototype.transform = function(operation) {
  * Pro Tip: Use "pass by reference" to alter the bitmap's buffer in place so you don't have to pass it around ...
  * @param bmp
  */
-const transformGreyscale = (bmp) => {
+const transformGreyscale = (img) => {
 
-  console.log('Transforming bitmap into greyscale', bmp);
+  console.log('transformGreyscale function on:', img);
 
   //TODO: Figure out a way to validate that the bmp instance is actually valid before trying to transform it
 
   //TODO: alter bmp to make the image greyscale ...
 
 };
+const changeColors = (bmp) => {
 
+}
 /**
  * A dictionary of transformations
  * Each property represents a transformation that someone could enter on the command line and then a function that would be called on the bitmap to do this job
@@ -66,14 +74,14 @@ function transformWithCallbacks() {
 
     bitmap.parse(buffer);
 
-    bitmap.transform(operation);
+    bitmap.transform('greyscale');
 
     // Note that this has to be nested!
     // Also, it uses the bitmap's instance properties for the name and thew new buffer
     fs.writeFile(bitmap.newFile, bitmap.buffer, (err, out) => {
-      if (err) {
-        throw err;
-      }
+      // if (err) {
+      //   throw err;
+      // }
       console.log(`Bitmap Transformed: ${bitmap.newFile}`);
     });
 
@@ -84,6 +92,5 @@ function transformWithCallbacks() {
 const [file, operation] = process.argv.slice(2);
 
 let bitmap = new Bitmap(file);
-
+// console.log(bitmap);
 transformWithCallbacks();
-
